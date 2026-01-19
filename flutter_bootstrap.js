4dd7bd47a078dfc08dfab983029b561ef7a59766
Mateusz Honc <mateuszhonc@gmail.com>
2026-01-19 21:53:59 +0100

    v0.1.1+4

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1753118799"
+    serviceWorkerVersion: "3123814304"
   }
 });
